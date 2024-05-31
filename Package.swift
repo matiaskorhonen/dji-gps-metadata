b526e8c4e93472815869babdeb3e2ec4e68bce9f
Matias Korhonen <me@matiaskorhonen.fi>
2024-05-31 20:57:58 +0300

    Lower the macOS requirement

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "DJIMetadataFixer",
   platforms: [
-    .macOS(.v14)
+    .macOS(.v12)
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0")
