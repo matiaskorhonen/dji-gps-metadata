62bad13b25aa23fbff1eaffe64491948129fa2d1
Matias Korhonen <me@matiaskorhonen.fi>
2024-05-31 22:40:49 +0300

    Update Test.swift

--- Tests/Test.swift
+++ Tests/Test.swift
@@ -1,7 +1,9 @@
+import DJIMetadataFixer
 import Testing
 
 @Suite struct SnakeOilTests {
   @Test func trueIsTrue() {
+    let _ = Exporter()  // Initialize the class just for kicks
     #expect(Bool(true))
   }
 }
