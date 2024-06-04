c693bb303d814e8b3b051a2b157b79956fefd485
Matias Korhonen <me@matiaskorhonen.fi>
2024-06-04 22:01:17 +0300

    Parse the FTYP fields

--- Sources/Atoms/FTYP.swift
+++ Sources/Atoms/FTYP.swift
@@ -6,19 +6,27 @@ extension Atom {
       let bytes = data[(data.startIndex + 4)..<(data.startIndex + 8)]
       return String(data: bytes, encoding: .utf8)
     }
-    var minorVersion: Decimal {
-      // TODO: convert BCD to Decimal
+    var minorVersion: String {
+      let bytes = [UInt8](data[(data.startIndex + 8)..<(data.startIndex + 12)])
+      let century = String(bytes[0], radix: 16, uppercase: true)
+      let year = String(bytes[1], radix: 16, uppercase: true)
+      let month = String(bytes[2], radix: 16, uppercase: true)
+      let zero = String(bytes[3], radix: 16, uppercase: true)
 
-      // let bytes = [UInt8](data[(data.startIndex + 8)..<(data.startIndex + 12)])
-
-      // let version = bytes.reduce(0) { soFar, byte in
-      //   return soFar << 8 | UInt32(byte)
-      // }
-
-      return 0
+      return [century + year, month, zero].joined(separator: ".")
     }
     var compatibleBrands: [String] {
-      return []
+      var cursor = data.startIndex + 12
+      var brands: [String?] = []
+      while cursor < data.endIndex {
+        let bytes = data[cursor..<(cursor + 4)]
+        guard !bytes.allSatisfy({ UInt8($0) == 0 }) else {
+          break
+        }
+        brands.append(String(data: bytes, encoding: .utf8))
+        cursor += 4
+      }
+      return brands.compactMap { $0 }
     }
     override var debugDescription: String {
       return """
