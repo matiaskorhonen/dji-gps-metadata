6e77e8edc3b165980c160a06f9864badd88959d6
Matias Korhonen <me@matiaskorhonen.fi>
2026-03-01 13:31:42 +0200

    Switch bamf dependency to remote Git URL

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
     .package(url: "https://github.com/apple/swift-testing.git", branch: "main"),
-    .package(path: "../bamf"),
+    .package(url: "https://github.com/matiaskorhonen/bamf.git", branch: "main"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
